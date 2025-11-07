aec76311ae87175d557fe0c538c56a7a3ba4613e
softmoca <acyc0909@gmail.com>
2025-11-07 17:36:46 +0900

    test : StringCalculator 기본 구분자로 합을 계산하는 테스트 추가

---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+
+
+    @Test
+    void 기본_구분자로_합을_계산한다() {
+        StringCalculator calculator = new StringCalculator();
+
+        int result = calculator.calculate("1,2:3");
+
+        assertThat(result).isEqualTo(6);
+    }
+
+}
