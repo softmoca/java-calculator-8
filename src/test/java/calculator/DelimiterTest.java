5c7e5e4dfa3c983e0107909390bf3cfc098632db
softmoca <acyc0909@gmail.com>
2025-11-07 17:28:03 +0900

    test : Delimiter 기본 구분자 파싱 테스트 추가

---
+++ src/test/java/calculator/DelimiterTest.java
@@ -0,0 +1,22 @@
+package calculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class DelimiterTest {
+
+    @Test
+    void 기본_구분자로_문자열을_분리한다() {
+        // given
+        Delimiter delimiter = Delimiter.getDefault();
+
+        // when
+        String[] result = delimiter.split("1,2:3");
+
+        // then
+        assertThat(result).containsExactly("1", "2", "3");
+    }
+
+
+}
