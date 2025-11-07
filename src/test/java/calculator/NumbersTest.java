44c82a8adbae7c1eb981d2a0dc8ae9a1dc0d0b16
softmoca <acyc0909@gmail.com>
2025-11-07 15:55:22 +0900

    test : 숫자들의 합 계산 테스트 추가
    - 어떻게 사용하지?
        - 가변인자?
        - List?
        - String 배열? → 문자열이 분리된채 넘어오니 가장 적합 !
    
    - String 배열을 Numbers로 변환
    - sum() 메서드로 합 계산
    
    - error : cannot resolve symbol 'Numbers'

---
+++ src/test/java/calculator/NumbersTest.java
@@ -0,0 +1,22 @@
+package calculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class NumbersTest {
+
+    @Test
+    void 숫자들의_합을_계산한다() {
+
+        // given
+        String[] tokens = {"1", "2"};
+
+        // when
+        Numbers numbers = Numbers.from(tokens);
+        int sum = numbers.sum();
+
+        // then
+        assertThat(sum).isEqualTo(3);
+    }
+}
