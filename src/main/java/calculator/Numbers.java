365290a0b26685eab1b43737c9a4491e81a9e5d7
softmoca <acyc0909@gmail.com>
2025-11-07 15:57:11 +0900

    feat : Numbers 기본 구조 및 하드 코딩 구현

---
+++ src/main/java/calculator/Numbers.java
@@ -0,0 +1,14 @@
+package calculator;
+
+public class Numbers {
+
+
+    public static Numbers from(String[] tokens) {
+        return new Numbers();
+    }
+
+    public int sum() {
+        return 3;
+    }
+
+}
