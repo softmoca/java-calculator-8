c4ada10547ad51334b707ccfd3277bc9e24b3095
softmoca <acyc0909@gmail.com>
2025-11-07 15:59:26 +0900

    test : Numbers 다른 숫자들의 합 테스트 추가
    org.opentest4j.AssertionFailedError:
    expected: 6
     but was: 3

--- src/test/java/calculator/NumbersTest.java
+++ src/test/java/calculator/NumbersTest.java
@@ -19,4 +19,15 @@ public class NumbersTest {
         // then
         assertThat(sum).isEqualTo(3);
     }
+
+    @Test
+    void 다른_숫자들의_합을_계산한다() {
+        String[] tokens = {"1", "2", "3"};
+
+        Numbers numbers = Numbers.from(tokens);
+
+        assertThat(numbers.sum()).isEqualTo(6);
+    }
+
+
 }
