b4f28727e03b452f8e6e188f76eea7237d4fea8e
softmoca <acyc0909@gmail.com>
2025-11-07 16:20:02 +0900

    test :Numbers 빈문자열 입력 시 예외 발생 테스트 추가

--- src/test/java/calculator/NumbersTest.java
+++ src/test/java/calculator/NumbersTest.java
@@ -39,4 +39,14 @@ public class NumbersTest {
                 .hasMessageContaining("음수");
     }
 
+
+    @Test
+    void 빈_문자열이_포함되면_예외가_발생한다() {
+        String[] tokens = {"1", "", "2"};
+
+        assertThatThrownBy(() -> Numbers.from(tokens))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자가 아닌 값");
+    }
+
 }
