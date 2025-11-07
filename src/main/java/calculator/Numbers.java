8d53cf59d73101cd64d977b887802b37fc034966
softmoca <acyc0909@gmail.com>
2025-11-07 16:08:13 +0900

    feat : Numbers 실제 합 계산 로직 구현
    - 하드코딩에서 실제 로직으로 변경
    - Stream을 사용한 합 계산

--- src/main/java/calculator/Numbers.java
+++ src/main/java/calculator/Numbers.java
@@ -1,14 +1,27 @@
 package calculator;
 
-public class Numbers {
+import java.util.Arrays;
+import java.util.List;
 
+public class Numbers {
+    private final List<Integer> values;
+
+    private Numbers(List<Integer> values) {
+        this.values = values;
+    }
 
     public static Numbers from(String[] tokens) {
-        return new Numbers();
+
+        List<Integer> numbers = Arrays.stream(tokens)
+                .map(Integer::parseInt)
+                .toList();
+
+        return new Numbers(numbers);
     }
 
     public int sum() {
-        return 3;
+        return values.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
     }
-
 }
