bdcf14a7aa74dfedb51b3b4af13494bd84312e7b
softmoca <acyc0909@gmail.com>
2025-11-08 11:54:03 +0900

    test : Expression의 Numbers 생성 통합테스트를 단위 테스트로 수정
    - Numbers는 값객체로서 equals를 구현
    - Expression에서 Number.from을 써도 되는 근거?
    	- 같은 레이어의 도메인 객체, 이미 단위테스트가 존재해서 검증됨, MOck보다 실제 객체가 낫다.
    	- 실제로도 함께 동작.

--- src/test/java/calculator/domain/ExpressionTest.java
+++ src/test/java/calculator/domain/ExpressionTest.java
@@ -57,12 +57,13 @@ class ExpressionTest {
         // given
         String input = "//;\\n1;2;3";
         Expression expression = Expression.from(input);
+        Numbers expected = Numbers.from(new String[]{"1", "2", "3"});
 
         // when
-        Numbers numbers = expression.toNumbers();
+        Numbers actual = expression.toNumbers();
 
         // then
-        assertThat(numbers.sum()).isEqualTo(6);
+        assertThat(actual).isEqualTo(expected);
     }
 
 
